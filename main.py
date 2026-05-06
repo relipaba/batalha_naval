a0f36b1fd1801f17f99ed25a8a6d0c9002a19810
relipaba <renato.6906@aluno.pr.senac.br>
2026-05-05 21:16:47 -0300

    arrumar erro de navio em for j in navio, no ataque

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 map_p1 = []
 map_p2 = []
+map_p1_atk = []
+map_p2_atk = []
 tamanho = 0
+turno = 1
 navios_p1 = [
     [],
     [],
@@ -33,8 +36,8 @@ for i in range(65):
 print("batalha naval 1d")
 while num_navio != 8:
     print(map_p1)
-    print("Jogador 1 começa, escolha qual navio escolher")
-    print(navios_p1, num_navio)
+    print("Jogador 1 começa, escolha qual navio colocar")
+    print(f"Casas ja selecionadas: {navios_p1}")
     opc = int(input(f"1 - navio de 9 casas ({navio9} restantes)\n2 - navio de 8 casas ({navio8} restantes)\n3 - navio de 5 casas ({navio5} restantes)\n4 - navio de 2 casas ({navio2} restantes)\n"))
     if opc == 1 and navio9 != 0:
         print("Escolha a posição do navio 9 casas")
@@ -88,6 +91,8 @@ while num_navio != 8:
         num_navio += 1
         navio2 -= 1
         navios -= 1
+        
+
 print("jogador 2, posicione seus navios")
 tamanho = 0
 num_navio = 0
@@ -96,8 +101,10 @@ navio8 = 2
 navio5 = 3
 navio2 = 2
 while num_navio != 8:
+
     print(map_p2)
-    print("Jogador 2 começa, escolha qual navio escolher")
+    print(f"Casas ja selecionadas: {navios_p2}")
+    print("Jogador 2, escolha qual navio colocar")
     opc = int(input(f"1 - navio de 9 casas ({navio9} restantes)\n2 - navio de 8 casas ({navio8} restantes)\n3 - navio de 5 casas ({navio5} restantes)\n4 - navio de 2 casas ({navio2} restantes)\n"))
     if opc == 1 and navio9 != 0:
         print("Escolha a posição do navio 9 casas")
@@ -152,6 +159,49 @@ while num_navio != 8:
         navio2 -= 1
         navios -= 1
 
-#Em seu turno, os jogadores tentam adivinhar a posição dos navios inimigos informando
-# a posição no mapa, caso acerte, exibir o tamanho do navio, caso erre, exibir 1
+fim = False
+for i in range(65):
+    map_p1_atk.append(0)
+    map_p2_atk.append(0)
 
+while fim == False:
+    while turno == 1:
+        print("turno do jogador 1 atacar")
+        print("este é o campo do jogador 2")
+        print(map_p2_atk)
+        pos = int(input("Selecione qual casa atacar"))
+        while pos < 0 or pos > 65 or map_p2[pos] == "X" or map_p2_atk[pos] == 1:
+            print(map_p2_atk)
+            pos = int(input("selecione outra casa, essa ou ja foi selecionada antes ou não existe no mapa"))
+        if map_p2[pos] == 1:
+            for i, navio in enumerate(navios_p2):
+                if pos in navio:
+                    print(f"Acertou um navio de {len(navio)} casas")
+                    for j in navio:
+                        map_p2[j] = "X"
+                        map_p2_atk[j] = "X"
+                        navio[j] = "X"
+        else:
+            print("nenhum navio acertado")
+            map_p1_atk[pos] = 1
+        turno = 2
+    while turno == 2:
+        print("turno do jogador 2 atacar")
+        print("este é o campo do jogador 1")
+        print(map_p1_atk)
+        pos = int(input("Selecione qual casa atacar"))
+        while pos < 0 or pos > 65 or map_p1[pos] == "X" or map_p1_atk[pos] == 1:
+            print(map_p1_atk)
+            pos = int(input("selecione outra casa, essa ou ja foi selecionada antes ou não existe no mapa"))
+        if map_p1[pos] == 1:
+            for i, navio in enumerate(navios_p1):
+                if pos in navio:
+                    print(f"Acertou um navio de {len(navio)} casas")
+                    for j in navio:
+                        map_p1[j] = "X"
+                        map_p1_atk[j] = "X"
+                        navio[j] = "X"
+        else:
+            print("nenhum navio acertado")
+            map_p1_atk[pos] = 1
+        turno = 1
